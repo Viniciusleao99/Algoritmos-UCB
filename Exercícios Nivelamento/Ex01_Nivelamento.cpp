ab794895a13fbc8584627725a9fc7ce7070de6e5
Vinicius Leão <111468790+Viniciusleao99@users.noreply.github.com>
2023-02-15 11:01:28 -0300

    Update Ex01_Nivelamento.cpp

--- Exercícios Nivelamento/Ex01_Nivelamento.cpp
+++ Exercícios Nivelamento/Ex01_Nivelamento.cpp
@@ -1,3 +1,7 @@
+/*Faça um programa em que o usuário digite o custo de uma determinada mercadoria, em seguida, adiciona-se ao custo o 
+valor do frete e despesas eventuais (também solicitadas pelo teclado).Para concluir, o programa pergunta qual o valor 
+de venda e indica a percentagem de lucro da mercadoria.*/
+
 #include<stdio.h>
 #include<string.h>
 #include<locale.h>
@@ -5,7 +9,7 @@
 int main(){
 setlocale(LC_ALL,"portuguese");
 
-float CustoMecadoria, CustoFrete, CustoDespesas, ValorFinal, ValorVenda, lucro;
+float CustoMecadoria, CustoFrete, CustoDespesas, ValorFinal, ValorVenda, lucro, PorcentagemLucro;
 
 printf("Digite o cuto da mercadoria:");
 scanf("%f", &CustoMecadoria);
@@ -16,12 +20,11 @@ scanf("%f", &CustoDespesas);
 
 ValorFinal = CustoMecadoria + CustoFrete + CustoDespesas;
 
-printf("O valor final do produto fica: %f", ValorFinal);
-
 printf("Digite o valor de venda:");
-scanf("%f", ValorVenda);
+scanf("%f", &ValorVenda);
 
 lucro = ValorVenda - ValorFinal;
+PorcentagemLucro = lucro / 100;
 
-printf("Logo o valor do lucro foi de %f", lucro);
+printf("Logo o valor do lucro foi de %.2f%", PorcentagemLucro);
 return 0;}
